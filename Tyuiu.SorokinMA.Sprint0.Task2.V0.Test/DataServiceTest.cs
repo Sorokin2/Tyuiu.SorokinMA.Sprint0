4bf5bf1456e19949d2d40f44a794d962c0a644d8
Tyuiu.SorokinMA@outlook.com <lorik.sorokin01@mail.ru>
2023-10-02 16:21:20 +0500

    Task2

---
+++ Tyuiu.SorokinMA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.SorokinMA.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.SorokinMA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessage()
+        {
+            var name = "Михаил";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет..., Михаил", res);
+        }
+    }
+}
