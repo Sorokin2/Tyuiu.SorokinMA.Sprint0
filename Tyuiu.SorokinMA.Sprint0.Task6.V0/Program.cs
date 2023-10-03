11fbc49c425d95c256daee4c8a31cc6be2af6900
Tyuiu.SorokinMA@outlook.com <lorik.sorokin01@mail.ru>
2023-10-03 12:08:51 +0500

    Task6

---
+++ Tyuiu.SorokinMA.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SorokinMA.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.SorokinMA.Sprint0.Task6.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numsArray = new int[] { 5, 2, 3, 4, 6 };
+            Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(numsArray));
+            Console.WriteLine("Разность элементов массива = " + DataService.SubtractionArray(numsArray));
+            Console.WriteLine("Произведение элементов массива = " + DataService.MultiplicationArray(numsArray));
+            Console.ReadKey();
+        }
+    }
+}
