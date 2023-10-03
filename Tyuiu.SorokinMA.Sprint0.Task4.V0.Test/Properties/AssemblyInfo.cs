6ada4691a1350defabe2a364bc67737b19ee1144
Tyuiu.SorokinMA@outlook.com <lorik.sorokin01@mail.ru>
2023-10-03 10:57:13 +0500

    Task4

---
+++ Tyuiu.SorokinMA.Sprint0.Task4.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SorokinMA.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SorokinMA.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("31e8deca-514b-404b-b76a-48e27748b23f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.SorokinMA.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SorokinMA.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.SorokinMA.Sprint0.Task4.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(7, 5));
+            Console.WriteLine(DataService.Subtraction(13, 5));
+            Console.WriteLine(DataService.Multiplication(4, 6));
+            Console.WriteLine(DataService.Division(5, 4));
+            Console.ReadKey();
+        }
+    }
+}
